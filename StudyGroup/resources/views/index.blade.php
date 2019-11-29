a55afa924da34038784fd897203d32f54914e912
Joshua Nishiguchi <jsn9@hawaii.edu>
2019-11-28 23:54:54 -1000

    Init basic

---
+++ StudyGroup/resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts/app')
+
+@section('content')
+<!-- Find Group page -->
+<div class="ui container" style="margin-top: 50px;">
+	<div class="ui grid">
+		<div class="eight wide right aligned column">
+			<div class="field">
+				<label>Class Name</label>
+				<input type="text" name="class-name" placeholder="ICS 415">
+			</div>
+		</div>
+		<div class="eight wide column">
+			test
+		</div>
+	</div>
+</div>
+
+@endsection
---
+++ StudyGroup/resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<script
+			src="https://code.jquery.com/jquery-3.1.1.min.js"
+			integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+			crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+		<script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+		<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+	</head>
+	<body>
+		<div class="ui inverted segment">
+			<div class="ui inverted secondary pointing menu">
+				<div class="ui container">
+					<a class="header item" href="{{ url('/') }}"><i class="book icon"></i>Study Group App</a>
+					@if (Request::is('/'))
+						<a class="active item" href="{{ url('/') }}">Find Group</a>
+						<a class="item" href="{{ url('/mygroups') }}">My Groups</a>
+					@elseif (Request::is('mygroups'))
+						<a class="item" href="{{ url('/') }}">Find Group</a>
+						<a class="active item" href="{{ url('/mygroups') }}">My Groups</a>
+					@else
+						<a class="item" href="{{ url('/') }}">Find Group</a>
+						<a class="item" href="{{ url('/mygroups') }}">My Groups</a>
+					@endif
+					@if (Auth::check())
+						<i class="user icon"></i>
+						<a class="right item" href="{{ url('/profile') }}">Welcome Back!</a>
+					@else
+						<a class="right item" href="{{ url('/login') }}">Login</a>
+					@endif
+				</div>
+			</div>
+		</div>
+		@yield('content')
+	</body>
+</html>
